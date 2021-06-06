5624c9bfaf1fec2ffa55af3a5797c9c4853af1e6
Nurlan-Khan <71606881+Nurlan-Khan@users.noreply.github.com>
2021-06-07 03:55:48 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function ChangeValue(number, pv) {
         number = number / 10
         j++
         if (number > 1 && j == 3) {
-            numberstring = "," + numberstring
+            numberstring = " " + numberstring
             j = 0
         }
         i++
@@ -25,7 +25,7 @@ function ChangeValue(number, pv) {
     numberstring = numberstring
 
     if (pv == 1) {
-        document.getElementById("worldpop").innerHTML = numberstring
+        document.getElementById("kazpop").innerHTML = numberstring
     }
 }
 
@@ -33,12 +33,14 @@ function ChangeValue(number, pv) {
 function now(year, month, date, hours, minutes, seconds) {
     startdatum = new Date(year, month, date, hours, minutes, seconds)
 
-    var now = 5600000000.0
-    var now2 = 5690000000.0
+    /* после получения новых данных на 1 число, меняем "var now" (1 мая), var now2 и месяц в schuldstartdatum (1 мая) */
+
+    var now = 18962892.0 /* 1 мая, текущая фактическая численность на 1 число */
+    var now2 = 19007850.0 /* 1 июля, прогноз на 1 число через 2 месяца (численность, к которой будет стремиться счетчик) */
     var groeipercentage = (now2 - now) / now * 100
-    var groeiperseconde = (now * (groeipercentage / 100)) / 365.0 / 24.0 / 60.0 / 60.0
+    var groeiperseconde = (now * (groeipercentage / 100)) / 60.0 / 24.0 / 60.0 / 60.0
     nu = new Date()
-    schuldstartdatum = new Date(96, 1, 1)
+    schuldstartdatum = new Date(2021, 4, 1) /* (год, месяц, число), дата, от которой идет отсчет счетчика,"4" это май*/
     secondenoppagina = (nu.getTime() - startdatum.getTime()) / 1000
     totaleschuld = (nu.getTime() - schuldstartdatum.getTime()) / 1000 * groeiperseconde + now
     ChangeValue(totaleschuld, 1);
@@ -48,68 +50,3 @@ function now(year, month, date, hours, minutes, seconds) {
 }
 
 window.onload = maind
-
-
-// chart.js
-// Population estimates: https://en.wikipedia.org/wiki/World_population_estimates
-
-// include better timeline with https://dima117.github.io/Chart.Scatter/ ?
-
-Chart.defaults.global.legend.display = false;
-
-var ctx = document.getElementById("myChart");
-var myChart = new Chart.Line(ctx, {
-    type: 'line',
-    data: {
-        labels: ['10,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '9,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '8,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '7,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '6,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '5,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '4,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '3,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '2,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1,000 BC', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '100', '200', '300', '400', '500', '600', '700', '800', '900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000', '2015', '2020', '2025', '2030', '2035', '2040', '2045', '2050'],
-        datasets: [{
-            data: ['2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '4', '4', '4', '4', '4', '4', '4', '4', '4', '4', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '8', '8', '8', '8', '8', '8', '8', '8', '8', '8', '11', '11', '11', '11', '11', '11', '11', '11', '11', '11', '18', '18', '18', '18', '18', '18', '18', '18', '18', '18', '28', '28', '28', '28', '28', '28', '28', '28', '28', '28', '45', '45', '45', '45', '45', '45', '45', '45', '45', '45', '72', '72', '72', '72', '72', '72', '72', '72', '72', '72', '115', '115', '115', '115', '115', '115', '115', '115', '150', '150', '188', '195', '202', '205', '209', '210', '213', '226', '240', '269', '295', '353', '393', '392', '390', '461', '554', '603', '989', '1654', '6089', '7256'],
-            fillColor: "rgba(151,205,187,0.2)",
-            strokeColor: "rgba(151,205,187,1)",
-            pointColor: "rgba(151,205,187,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,205,187,1)",
-        }, {
-            data: ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', '7256', '7758', '8142', '8501', '8839', '9157', '9454', '9725'],
-            fillColor: "rgba(205,151,187,0.2)",
-            strokeColor: "rgba(205,151,187,1)",
-            pointColor: "rgba(205,151,187,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(205,151,187,1)",
-        }]
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                position: "bottom",
-                time: {
-                    // string/callback - By default, date objects are expected. You may use a pattern string from https://momentjs.com/docs/#/parsing/string-format/ to parse a time string format, or use a callback function that is passed the label, and must return a moment() instance.
-                    parser: false,
-                    // string - By default, unit will automatically be detected.  Override with 'week', 'month', 'year', etc. (see supported time measurements)
-                    unit: false,
-
-                    // Number - The number of steps of the above unit between ticks
-                    unitStepSize: 1,
-
-                    // string - By default, no rounding is applied.  To round, set to a supported time unit eg. 'week', 'month', 'year', etc.
-                    round: false,
-
-                    // Moment js for each of the units. Replaces `displayFormat`
-                    // To override, use a pattern string from https://momentjs.com/docs/#/displaying/format/
-                    displayFormats: {
-                        'year': 'YYYY', // 2015
-                    },
-                    // Sets the display format used in tooltip generation
-                    tooltipFormat: ''
-                },
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
