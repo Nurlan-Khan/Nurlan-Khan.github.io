91eff5e83f8ec08fea1e2471d598572cc31651c3
Nurlan-Khan <71606881+Nurlan-Khan@users.noreply.github.com>
2021-06-15 03:40:31 +0600

    Add files via upload

--- script_global.js
+++ script_global.js
@@ -1,17 +1,3 @@
 window.odometerOptions = {
     format: " ddd", // https://github.hubspot.com/odometer/ Change how digit groups are formatted, and how many digits are shown after the decimal point
 };
-
-
-
-function supportsImports() {
-    return 'import' in document.createElement('link');
-}
-
-if (supportsImports()) {
-    // Good to go!
-} else {
-    // Use other libraries/require systems to load files.
-}
-
-var doc = document.querySelector('link[rel="import"]').import
