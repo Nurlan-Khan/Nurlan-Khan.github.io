1d85f6f57b2da41fe8ea6c0865aedb23058db625
Nurlan-Khan <71606881+Nurlan-Khan@users.noreply.github.com>
2021-07-07 10:07:52 +0600

    Delete script_global.js

--- script_global.js
+++
@@ -1,3 +0,0 @@
-window.odometerOptions = {
-    format: " ddd", // https://github.hubspot.com/odometer/ Change how digit groups are formatted, and how many digits are shown after the decimal point
-};
